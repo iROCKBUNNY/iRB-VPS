95e86f7f46315378c6e413d0073dde0a5f4e09b6
iRB Air <irockbunny@outlook.com>
2017-03-25 01:51:23 +0800

    Update

--- assets/js/set-footer.js
+++ assets/js/set-footer.js
@@ -4,7 +4,7 @@ $.getJSON('https://vps.irockbunny.com/json/footer-data.json', function(data) {
     $('#footer-info').append($('<p>').attr('id', 'powered-by').html('Powered by '));
     for (var i in data.powered_by) {
         if (i < 1) {
-            $('#powered-by').append($('<a>').attr('href', data.powered_by[i].link).attr('target', '_blank').html(data.powered_by[i].name)).append(' ' + data.powered_by[i].version);
+            $('#powered-by').append($('<a>').attr('href', data.powered_by[i].link).attr('target', '_blank').html(data.powered_by[i].name));
         }
         else {
             $('#powered-by').append(' &middot; ').append($('<a>').attr('href', data.powered_by[i].link).attr('target', '_blank').html(data.powered_by[i].name));
